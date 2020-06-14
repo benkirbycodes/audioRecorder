f4932f4e1b4208fb9743c581f6fdd39b5ab18038
benkirbycodes <benkirbycodes@gmail.com>
2020-06-14 12:12:25 -0600

    Initial Commit

--- audioRecorder/ViewController.swift
+++ audioRecorder/ViewController.swift
@@ -7,10 +7,93 @@
 //
 
 import UIKit
+import AVFoundation
+var audioRecorder: AVAudioRecorder!
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+
+    var recordButton: UIButton!
+    var recordingSession: AVAudioSession!
+    
+    @objc func recordTapped() {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask )
+        return paths[0]
+    }
+ 
+    func loadRecordingUI() {
+        recordButton = UIButton(frame: CGRect(x: 64, y: 64, width: 128, height: 64))
+        recordButton.setTitle("Tap to record", for: .normal)
+        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+        view.addSubview(recordButton)
+    }
+    
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.mp4a")
+        print(audioFilename)
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do{
+            try audioRecorder = AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Tap to record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to record", for: .normal)
+            print("Recording failed")
+        }
+    }
+     override func viewDidLoad() {
+        
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        print("Failed to Record!")
+                    }
+                }
+            }
+        } catch {
+            print(error)
+        }
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
